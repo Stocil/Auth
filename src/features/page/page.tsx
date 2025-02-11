1df066e4d67d814591ed9285a71243118caa78ff
Prikop3sm <900543@list.ru>
2025-02-11 21:41:37 +0300

    добавил хедер

--- src/features/page/page.tsx
+++ src/features/page/page.tsx
@@ -1,16 +1,34 @@
 import { FC, HTMLAttributes } from 'react';
 
+import { useNavigate } from 'react-router';
+
+import { AppBar } from 'components/app-bar';
+
+import { routesPaths } from 'routes/routes.tsx';
+
 import { Container, PageStyled } from './page-styles';
 
 type Props = HTMLAttributes<HTMLDivElement>;
 
 export const Page: FC<Props> = ({ children }) => {
+  const navigate = useNavigate();
+  const isLogin = false; // TODO: Сделать определение логина
+
+  const onClick = () => {
+    if (isLogin) {
+      // TODO: Сделать logout
+      return;
+    }
+    navigate(routesPaths.signIn);
+  };
+
   return (
-    <PageStyled>
-      {
-        // TODO: Добавить хедер
-      }
-      <Container>{children}</Container>
-    </PageStyled>
+    <>
+      <AppBar isLogin={isLogin} onClick={onClick} />
+
+      <PageStyled>
+        <Container>{children}</Container>
+      </PageStyled>
+    </>
   );
 };
