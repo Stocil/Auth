2aa1faa7fd87faef8c2b58d277dbd73b8319b3b1
Stas Vashurov <88900812+Stocil@users.noreply.github.com>
2025-02-18 00:20:22 +0300

    Пофиксил вход на BE, добавил access провайдер на FE (#23)

--- backend/src/api/user/sign-up.ts
+++ backend/src/api/user/sign-up.ts
@@ -25,6 +25,6 @@ export const signUp = (req: Request, res: Response) => {
   addUserToDB(user);
   const { accessToken, refreshToken } = generateTokens(userJWTData);
 
-  res.cookie('token', refreshToken, { httpOnly: true });
-  res.json(accessToken);
+  console.log('Register user:', user);
+  res.cookie('token', refreshToken, { httpOnly: true }).json(accessToken);
 };
--- frontend/src/store/user/types.ts
+++ frontend/src/store/user/types.ts
@@ -3,7 +3,6 @@ import { Nullable } from 'types';
 export type UserInfo = {
   login: string;
   email: string;
-  password: string;
 };
 
 export type InitialState = {
