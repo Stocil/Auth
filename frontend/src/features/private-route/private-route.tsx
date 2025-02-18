50b1f37c192844f430c619e7762815303fbbe4e8
Stocil <vashurov02@mail.ru>
2025-02-19 00:19:04 +0300

    Добавил ручку /logout на BE, пофиксил access провайдер и приватный роут на FE

--- frontend/src/features/private-route/private-route.tsx
+++ frontend/src/features/private-route/private-route.tsx
@@ -6,7 +6,9 @@ import { useLocation, useNavigate } from 'react-router';
 import { authApi } from 'store/api/auth';
 import { getUserIsLoggin } from 'store/user/selectors';
 
-import { routesPaths } from '../../routes/routes';
+import { routesPaths } from 'routes/routes';
+
+import { getTokenFromCookie } from 'utils/token';
 
 type Props = PropsWithChildren;
 
@@ -14,19 +16,32 @@ export const PrivateRoute: FC<Props> = ({ children }) => {
   const { isSuccess, isError } =
     authApi.endpoints.checkUserAccess.useQueryState();
 
-  const isAccessFetched = isSuccess || isError;
+  const isLoading = !isSuccess && !isError;
+
+  const token = getTokenFromCookie();
   const isLogin = useSelector(getUserIsLoggin);
   const navigate = useNavigate();
   const { pathname } = useLocation();
 
+  // Подвязываем на isLogin, чтобы происходил редирект
+  // при выходе из аккаунта в приватном роуте
+  let isPageAvailable = isLogin;
+
+  if (token) {
+    const isAccessSuccess = isSuccess ? true : false;
+
+    // Держим пользователя на этой странице до тех пор, пока запрос не завершится
+    isPageAvailable = isLoading ? true : isAccessSuccess;
+  }
+
   useEffect(() => {
-    if (!isLogin && isAccessFetched && isError) {
+    if (!isPageAvailable) {
       navigate(
         { pathname: routesPaths.signIn },
         { state: { prevPath: pathname } },
       );
     }
-  }, [isLogin, pathname, navigate, isAccessFetched]);
+  }, [isPageAvailable]);
 
   return children;
 };
