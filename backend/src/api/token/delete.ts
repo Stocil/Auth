b6a1ddf9324a28c918eff6825c3012601b696cdf
Stocil <vashurov02@mail.ru>
2025-02-20 23:37:26 +0300

    Фикс

--- backend/src/api/token/delete.ts
+++ backend/src/api/token/delete.ts
@@ -3,8 +3,5 @@ import { Request, Response } from 'express';
 import { HTTP_NO_CONTENT } from 'constants/http-codes';
 
 export const deleteToken = (_: Request, res: Response) => {
-  res
-    .clearCookie('token')
-    .clearCookie('cookieToken')
-    .sendStatus(HTTP_NO_CONTENT);
+  res.clearCookie('cookieToken').sendStatus(HTTP_NO_CONTENT);
 };
