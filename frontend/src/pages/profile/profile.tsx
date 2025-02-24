b4c457d145c00b32b76566b681b99eb7e4183210
Stocil <vashurov02@mail.ru>
2025-02-25 02:20:06 +0300

    Добавил page wrapper, добавил кастомный size к кнопке, добавил лодер с приватный роут

--- frontend/src/pages/profile/profile.tsx
+++ frontend/src/pages/profile/profile.tsx
@@ -1,7 +1,24 @@
 import { FC } from 'react';
 
 import { Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+
+import { getUserInfo } from 'store/user/selectors';
+
+import { PageWrapper } from 'components/page-wrapper';
+
+import { PageEmptyPage } from './empty-page';
 
 export const Profile: FC = () => {
-  return <Typography variant='h3'>Профиль</Typography>;
+  const userData = useSelector(getUserInfo);
+
+  return (
+    <PageWrapper
+      isLoading={false}
+      isEmptySearch={!userData}
+      noDataFallback={<PageEmptyPage />}
+    >
+      <Typography variant='h3'>Профиль</Typography>
+    </PageWrapper>
+  );
 };
