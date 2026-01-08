e6a4d7ea42349157cb0614a87991c602fbd82b19
Stas Vashurov <vashurov02@mail.ru>
2026-01-08 14:52:54 +0300

    FE - Добавил transition инпуту в профиле

--- frontend/src/pages/profile/form/constants.ts
+++ frontend/src/pages/profile/form/constants.ts
@@ -1,10 +1,8 @@
 import { SxProps, Theme } from '@mui/material';
 
 export const profileDirtyFieldSx: SxProps<Theme> = {
-  '.MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
-    borderColor: ({ palette }) => palette.success.main,
-  },
   '.MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
     borderColor: ({ palette }) => palette.success.main,
+    transition: 'border-color 0.5s',
   },
 };
